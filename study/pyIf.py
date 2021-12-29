0be77d91602ebcbd8e86f1f2bf55629827a1511c
轩辕龙儿 <hyy>
2021-12-29 16:39:11 +0800

    博客例子：python3学习笔记--条件控制用法整理

---
+++ study/pyIf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @author JourWon
+# @date 2021/12/29
+# @file pyIf.py
+if __name__ == "__main__":
+    n = 10
+    if n < 0:
+        print(-1)
+    elif n == 0:
+        print(0)
+    else:
+        print(1)
