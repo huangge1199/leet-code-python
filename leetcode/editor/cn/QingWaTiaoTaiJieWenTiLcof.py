6cea9e856499c1f24382bad2a44156136be5252f
轩辕龙儿 <hyy>
2022-01-07 16:06:32 +0800

    1614:括号的最大嵌套深度

--- leetcode/editor/cn/QingWaTiaoTaiJieWenTiLcof.py
+++
@@ -1,5 +0,0 @@
-# 剑指 Offer 10- II:青蛙跳台阶问题
-# leetcode submit region begin(Prohibit modification and deletion)
-class Solution:
-    def numWays(self, n: int) -> int:
-# leetcode submit region end(Prohibit modification and deletion)
