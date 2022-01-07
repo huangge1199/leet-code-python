d9d562721d3a623416a54bb781d0ed7ea2e7ff16
轩辕龙儿 <hyy>
2022-01-07 10:56:56 +0800

    剑指 Offer 05:替换空格

---
+++ leetcode/editor/cn/TiHuanKongGeLcof.py
@@ -0,0 +1,6 @@
+# 剑指 Offer 05:替换空格
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        return s.replace(" ", "%20")
+# leetcode submit region end(Prohibit modification and deletion)
